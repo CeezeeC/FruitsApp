8e33deebc06912610650c09de26c2f9b09bcc9c2
CecilMkhari(LubanziA <CecilMkhari(LubanziA@DESKTOP-8DS478L>
2025-05-20 16:16:22 +0200

    Add project files.

---
+++ FruitsApp/FruitsField.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitsApp
+{
+    public  class FruitsField
+    {
+
+        private string[] fruits;// private field
+        internal int choice;// internal field
+
+        // it allows user to pick fruits 3 times
+        public int turns = 0;//public filed
+
+        protected int exit = 3;
+
+
+
+        //method with an array parameter 
+        //method allows user to pick fruits
+        public  void PickMethod(string[] fruits)
+        {
+            
+            do
+            {
+                try
+                {
+                    Console.Write("Enter a number from 1 to 6 to pick a fruit : ");
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    // check if the entered choice is out of the range of the array
+                    if (choice >= 1 && choice <= fruits.Length)
+                    {
+                        //check if the fruits has not been eaten
+                        if (fruits[choice - 1] == "Eaten")
+                        {
+                            Console.WriteLine(" OOPS!! this has already been eaten choose another one ");
+                        }
+                        else
+                        {
+                            Console.WriteLine(fruits[choice - 1] + " Has been eaten ");
+                            fruits[choice - 1] = "Eaten";
+                            turns++;//increament, 
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine(" Entered number is out of the range : ");
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+
+            } while (turns < 3);// it repeats until 3 times
+        }
+
+        //Display current result using for loop
+        public  void PrintMethod(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine((i + 1) + "." + array[i]);
+            }
+        }
+
+
+        
+    }
+}
---
+++ FruitsApp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitsApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // creating class object
+            FruitsField fruits1 = new FruitsField();
+
+            bool exit = false;
+            // Creating an Array of six element the size
+            String[] fruits = new String[6]{ "Banana", "Blueberry", "Cherry", "Apple", "Cucumber", "Orange " };
+            
+            //begining  of the main loop
+            //menu will pop up until you choose to exit
+            while (!exit)
+            {
+
+                Console.WriteLine("***********FRUITS QUICK PICK**********");
+                //handle error and expections
+                try
+                {
+                    //Display main menu
+                    Console.WriteLine("Please enter (1) to Pick >>>\n"+
+                                      "Please entet (2) to Display >>>\n"+
+                                      "Please enter (3) to Exit >>> ");
+                    int choice = Convert.ToInt32(Console.ReadLine());// choice from user input
+
+                    // the entry of switch statement
+                    switch (choice)
+                    {
+                        case 1:
+
+                            fruits1.PickMethod(fruits);// call method to pick fruit
+                            break;
+
+                        case 2:
+
+                            fruits1.PrintMethod(fruits);// call method to display fruit
+
+                            break;
+                        case 3:
+
+                            Console.WriteLine("You have choosen to exit Goodbye !!!!!!!!!");
+                            exit = true;// exit the loop
+                            break;  
+
+                            default:
+                            Console.WriteLine(" Incorrect choice ,enter number from 1 to 3 ");
+                            break;
+                    }
+                }
+                // catch error and display messege
+                catch (Exception ex)
+                {
+                    
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        
+      
+    }
+}
+        
+    
+
